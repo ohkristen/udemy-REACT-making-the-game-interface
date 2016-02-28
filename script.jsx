2ca31d793b9f32dd753213c02ee8f4db6f59410a
Kristen Gunn <ohkristen@yahoo.com>
2016-02-28 15:25:20 -0800

    created blank div components for Stars box, Button and AnswerFrame and referenced them in the main Game component

--- script.jsx
+++ script.jsx
@@ -1,14 +1,45 @@
-var Game = React.createClass({
+var Stars = React.createClass({
   render: function() {
     return (
-      <div id="game">
-        <h2>Play Nine</h2>
+      <div>
+      ...
       </div>
     );
   }
 });
 
+var Button = React.createClass({
+  render: function() {
+    return (
+      <div>
+      ...
+      </div>
+    );
+  }
+});
 
+var AnswerFrame = React.createClass({
+  render: function() {
+    return (
+      <div>
+      ...
+      </div>
+    );
+  }
+});
+
+var Game = React.createClass({
+  render: function() {
+    return (
+      <div id="game">
+        <h2>Play Nine</h2>
+        < Stars />
+        < Button />
+        < AnswerFrame />
+      </div>
+    );
+  }
+});
 
 React.render(
   < Game />,
