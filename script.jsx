85568d9a253c967104e5bc35af8b60a8f778acb9
Kristen Gunn <ohkristen@yahoo.com>
2016-03-01 13:25:38 -0800

    render 5 stars as a test, and push the number of stars into an array after each iteration of a for loop

--- script.jsx
+++ script.jsx
@@ -1,13 +1,18 @@
 var StarsFrame = React.createClass({
   render: function() {
+
+    var numberOfStars = 5;
+    var stars = [];
+
+    for (var i=0; i<numberOfStars; i++){
+      stars.push(
+        <span className="glyphicon glyphicon-star"></span>
+      )
+    }
     return (
       <div id="stars-frame">
         <div className="well">
-          <span className="glyphicon glyphicon-star"></span>
-          <span className="glyphicon glyphicon-star"></span>
-          <span className="glyphicon glyphicon-star"></span>
-          <span className="glyphicon glyphicon-star"></span>
-          <span className="glyphicon glyphicon-star"></span>
+          {stars}
         </div>
       </div>
     );
