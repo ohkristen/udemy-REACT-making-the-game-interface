a36aa88715cd0622e3ad04bdc5dce07f829c3c71
Kristen Gunn <ohkristen@yahoo.com>
2016-03-03 13:33:03 -0800

    allow a player to change the selected number in the answers frame, if they make a mistake.  first step - change all references to clickNumber to selectNumber

--- script.jsx
+++ script.jsx
@@ -45,13 +45,13 @@ var NumbersFrame = React.createClass({
   render: function() {
 
       var numbers = [], className,
-        clickNumber=this.props.clickNumber;
+        selectNumber=this.props.selectNumber;
         selectedNumbers=this.props.selectedNumbers;
 
       for (var i=1; i<=9; i++){
         className="number selected-" + (selectedNumbers.indexOf(i)>=0);
         numbers.push(
-          <div className={className} onClick={clickNumber.bind(null, i)}>
+          <div className={className} onClick={selectNumber.bind(null, i)}>
           {i}
           </div>
         );
@@ -73,7 +73,7 @@ var Game = React.createClass({
             selectedNumbers: []
     }
   },
-  clickNumber: function(clickedNumber) {
+  selectNumber: function(clickedNumber) {
     if(this.state.selectedNumbers.indexOf(clickedNumber) < 0){
     this.setState(
     {selectedNumbers: this.state.selectedNumbers.concat(clickedNumber)}
@@ -91,7 +91,7 @@ var Game = React.createClass({
             < AnswerFrame selectedNumbers={this.state.selectedNumbers} />
         </div>
             < NumbersFrame selectedNumbers={this.state.selectedNumbers}
-                            clickNumber={this.clickNumber} />
+                            selectNumber={this.selectNumber} />
       </div>
     );
   }
